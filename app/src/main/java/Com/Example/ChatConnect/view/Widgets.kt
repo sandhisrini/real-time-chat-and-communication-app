3504a01c2739bd268a91ed5b676c26c6ecf9657e
sandhisrini <sandhisrini5@gmail.com>
2024-11-22 15:59:32 +0530

    Update Widgets.kt

--- app/src/main/java/Com/Example/ChatConnect/view/Widgets.kt
+++ app/src/main/java/Com/Example/ChatConnect/view/Widgets.kt
@@ -1,1 +1,156 @@
+package com.example.chatconnect.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Title(title: String) {
+    Text(
+        text = title,
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color= Color(242, 252, 138),
+        modifier = Modifier.fillMaxHeight(0.3f)
+    )
+}
+
+// Different set of buttons in this page
+@Composable
+fun Buttons(title: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(200.dp)
+            .padding(bottom = 16.dp),
+        shape = RoundedCornerShape(50),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color(242, 252, 138),
+            contentColor = Color(32, 35, 43)
+        )
+    ) {
+        Text(
+            text = title,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(5.dp)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Appbar(title: String, action: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(text = title,
+                color=Color(221, 208, 242))
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = action
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back button",
+                    tint = Color(221, 208, 242)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color(32, 35, 43))
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFormField(value: String, onValueChange: (String) -> Unit, label: String, keyboardType: KeyboardType, visualTransformation: VisualTransformation) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(
+                label,
+                color= Color(221, 208, 242)
+            )
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color(34, 34, 43) ,
+            unfocusedBorderColor = Color(218, 199, 244)),
+        maxLines = 1,
+        modifier = Modifier
+            .padding(horizontal = 20.dp, vertical = 5.dp)
+            .fillMaxWidth(),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType
+        ),
+        singleLine = true,
+        visualTransformation = visualTransformation
+    )
+}
+
+@Composable
+fun SingleMessage(message: String, isCurrentUser: Boolean) {
+    val cardColor = if(isCurrentUser) Color(91, 82, 101) else Color(22, 23, 27)
+    val cardArrangeemnt = if (isCurrentUser) Arrangement.End else Arrangement.Start
+    val cardAlignment = if (isCurrentUser) Alignment.End else Alignment.Start
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 5.dp),
+        horizontalArrangement = cardArrangeemnt
+    ) {
+        Card(
+            shape = RoundedCornerShape(16.dp)
+            ) {
+            Column(
+                Modifier
+                    .background(cardColor)
+                    .widthIn(0.dp,300.dp),
+                horizontalAlignment = cardAlignment
+            ) {
+                Text(
+                    text = message,
+                    modifier = Modifier
+                        .padding(16.dp),
+                    color = Color(221, 208, 242),
+                    fontSize = 20.sp,
+                )
+            }
+        }
+    }
+}
