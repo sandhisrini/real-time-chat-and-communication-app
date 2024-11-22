0ea454b4442f918768cf3688d14cf7e17986d664
sandhisrini <sandhisrini5@gmail.com>
2024-11-22 16:00:13 +0530

    Update Home.kt

--- app/src/main/java/Com/Example/ChatConnect/view/home/Home.kt
+++ app/src/main/java/Com/Example/ChatConnect/view/home/Home.kt
@@ -1,1 +1,140 @@
+package com.example.chatconnect.view.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.chatconnect.Constants
+import com.example.chatconnect.view.SingleMessage
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeView(
+    homeViewModel: HomeViewModel = viewModel()
+) {
+    val message: String by homeViewModel.message.observeAsState(initial = "")
+    val messages: List<Map<String, Any>> by homeViewModel.messages.observeAsState(
+        initial = emptyList<Map<String, Any>>().toMutableList()
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(32, 35, 43)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        AppbarMain(
+            title = "ChatConnect"
+        )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(weight = 0.85f, fill = true),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            reverseLayout = true
+        ) {
+            items(messages) { message ->
+                val isCurrentUser = message[Constants.IS_CURRENT_USER] as Boolean
+
+                SingleMessage(
+                    message = message[Constants.MESSAGE].toString(),
+                    isCurrentUser = isCurrentUser
+                )
+            }
+        }
+        OutlinedTextField(
+            value = message,
+            onValueChange = {
+                homeViewModel.updateMessage(it)
+            },
+            shape = RoundedCornerShape(16.dp),
+            label = {
+                Text(
+                    "Type Your Message",
+                    color= Color(221, 208, 242)
+                )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color(218, 199, 244) ,
+                unfocusedBorderColor = Color(218, 199, 244),
+                textColor = Color(221, 208, 242)
+            ),
+            maxLines = 1,
+            modifier = Modifier
+                .padding(horizontal = 15.dp, vertical = 1.dp)
+                .fillMaxWidth()
+                .weight(weight = 0.09f, fill = true),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text
+            ),
+            singleLine = true,
+            trailingIcon = {
+                IconButton(
+                    onClick = {
+                        homeViewModel.addMessage()
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Send,
+                        contentDescription = "Send Button",
+                        tint = Color(221, 208, 242)
+                    )
+                }
+            }
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppbarMain(title: String) {
+    TopAppBar(
+        scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(),
+        title = {
+            Text(text = title,
+                color= Color(221, 208, 242)
+            )
+        },
+//        navigationIcon = {
+//            IconButton(
+//                onClick = action
+//            ) {
+//                Icon(
+//                    imageVector = Icons.Filled.ArrowBack,
+//                    contentDescription = "Back button",
+//                    tint = Color(221, 208, 242)
+//                )
+//            }
+//        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color(23, 23, 29))
+    )
+}
